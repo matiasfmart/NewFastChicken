5f73b121689276c551737736e9588b44474bb25e
Matias Martinez <martinez.matiashc@gmail.com>
2025-11-02 03:25:15 +0000

    Try fixing this error: `Console Error: [2025-11-02T03:24:29.861Z] @fireb

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -3,9 +3,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  authDomain: "fast-chicken-pos.firebaseapp.com",
+  projectId: "fast-chicken-pos",
+  storageBucket: "fast-chicken-pos.appspot.com",
+  messagingSenderId: "108343513425",
+  appId: "1:108343513425:web:c81535496423932468d669"
 };
+
