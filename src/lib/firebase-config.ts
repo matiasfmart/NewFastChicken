a7ca92c0a5afb5f0c815e5dfa233ad51fb783d52
CENTRAL\203681 <203681@coto.com.ar>
2025-11-03 23:15:09 -0300

    update configs

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -2,11 +2,10 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyC9zaeFPWm91sTnq4ZqJGwMTpm-mA6EyrM",
-  authDomain: "fastchicken-f46f0.firebaseapp.com",
-  projectId: "fastchicken-f46f0",
-  storageBucket: "fastchicken-f46f0.firebasestorage.app",
-  messagingSenderId: "571983363320",
-  appId: "1:571983363320:web:15f6e9987a622104254f0d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
-
